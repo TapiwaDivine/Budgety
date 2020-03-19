79d0c13152a2dcbe646adb820409cc0d27451a8f
TapiwaDivine <tdchipatiko07@gmail.com>
2020-03-19 20:24:32 +0000

    displaying percentages on each item

--- app.js
+++ app.js
@@ -174,7 +174,8 @@ var UIController = (function(){
         incomeLabel: '.budget__income--value',
         expenseLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage',
 
     };
     return {
@@ -246,6 +247,30 @@ var UIController = (function(){
             
         },
 
+        displayPercentanges = function (percentages) {
+
+            var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+            var nodeListForEach = function(list, callback){
+
+                for(var i = 0; i < list.length; i++){
+                    callback(list[i], i);
+                }
+            };
+
+            nodeListForEach(fields, function(current, index){
+                
+                if(percentages[index] > 0){
+                    current.textContent = percentages[index] + '%';
+                }else {
+                    current.textContent = '---'
+                }
+            });
+
+
+
+        },
+
         // globalizing the DOMstring object to acces it in the controller___DOMSTINGS TO CONTROLER
         getDOMstrings: function(){
             return DOMstrings;
@@ -295,7 +320,7 @@ var controller = (function(budgetCtrl, UICtrl){
         var percentages = budgetCtrl.getPercentages();
 
         // 3. Update UI with the new percentages
-        console.log(percentages)
+        UICtrl.displayPercentanges(percentages)
     }
 
     // Controller function for adding items_______________________________CTRL ADD ITEMS
